731a3a895c8e3d7d216859c46e27e8ecee9f4550
Dave Naspinski <djnaz@mac.com>
2023-04-14 20:05:30 -0400

    Added Submenus to Main Nav

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,76 +1,71 @@
-import React from 'react'
-import { graphql, Link, useStaticQuery } from 'gatsby'
+import React from "react";
+import { graphql, Link, useStaticQuery } from "gatsby";
 
 export default function MainNav() {
-
   // This returns a flat list of menu items
   const data = useStaticQuery(graphql`
-  {
-    allWpMenuItem(sort: {order: ASC}, filter: {locations: {eq: MAIN_MENU}}) {
-      nodes {
-        id
-        path
-        label
-        parentId
+    {
+      allWpMenuItem(
+        sort: { order: ASC }
+        filter: { locations: { eq: MAIN_MENU } }
+      ) {
+        nodes {
+          id
+          path
+          label
+          parentId
+        }
       }
     }
-  }
-  `)
+  `);
 
-  console.log(data.allWpMenuItem.nodes)
+  console.log(data.allWpMenuItem.nodes);
 
   // This functions converts the flat menu list into a hierarchical list
-  const flatListToHierarchical = (data = [], {idKey='id',parentKey='parentId',childrenKey='children'} = {}) => {
-      const tree = [];
-      const childrenOf = {};
-      data.forEach((item) => {
-          const newItem = {...item};
-          const { [idKey]: id, [parentKey]: parentId = 0 } = newItem;
-          childrenOf[id] = childrenOf[id] || [];
-          newItem[childrenKey] = childrenOf[id];
-          parentId
-              ? (
-                  childrenOf[parentId] = childrenOf[parentId] || []
-              ).push(newItem)
-              : tree.push(newItem);
-      });
-      return tree;
+  const flatListToHierarchical = (
+    data = [],
+    { idKey = "id", parentKey = "parentId", childrenKey = "children" } = {}
+  ) => {
+    const tree = [];
+    const childrenOf = {};
+    data.forEach((item) => {
+      const newItem = { ...item };
+      const { [idKey]: id, [parentKey]: parentId = 0 } = newItem;
+      childrenOf[id] = childrenOf[id] || [];
+      newItem[childrenKey] = childrenOf[id];
+      parentId
+        ? (childrenOf[parentId] = childrenOf[parentId] || []).push(newItem)
+        : tree.push(newItem);
+    });
+    return tree;
   };
 
-  const mainMenuList = flatListToHierarchical(data.allWpMenuItem.nodes)
+  const mainMenuList = flatListToHierarchical(data.allWpMenuItem.nodes);
 
-  console.log(mainMenuList)
+  console.log(mainMenuList);
 
   return (
-    <nav className='main-menu'>
-      <ul className='menu'>
-
-      { mainMenuList.map(menuItem => (
+    <nav className="main-menu">
+      <ul className="menu">
+        {mainMenuList.map((menuItem) => (
           <li>
-            <Link to={ menuItem.path } key={ menuItem.id }>{ menuItem.label }</Link>
-            { menuItem.children.map(childItem => (
-              <li>
-                <Link to={ childItem.path } key={ childItem.id }>{ childItem.label }</Link>
-              </li>
-            ))}
+            <Link to={menuItem.path} key={menuItem.id}>
+              {menuItem.label}
+            </Link>
+            {menuItem.children.length > 0 ? (
+              <ul className="sub-menu">
+                {menuItem.children.map((childItem) => (
+                  <li>
+                    <Link to={childItem.path} key={childItem.id}>
+                      {childItem.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
           </li>
         ))}
-
-        {/* <li>
-        <Link to="/">Home</Link>
-        </li>
-        <li>
-        <Link to="/demo/">Demo</Link>
-        <ul className='sub-menu'>
-          <li>
-          <Link to="/demo/homepage">Homepage</Link>
-          </li>
-          <li>
-          <Link to="/demo/blog">Blog</Link>
-          </li>
-        </ul>
-        </li> */}
       </ul>
     </nav>
-  )
+  );
 }
